955091b7ba4511f2d11678de40bf172ee86aedc3
Alex Kraken Hawthorne <19832622+AlexKraken@users.noreply.github.com>
2020-06-04 23:11:40 -0500

    Replace rectangles with color pickers and remove uneccessary functions
    Instead of trying to pair rectangles with a color picker, color pickers 
    are instead resized to the desired flag dimensions. Refactored how the 
    interface updates to reuse code and be more concise.

--- sketch.js
+++ sketch.js
@@ -1,14 +1,42 @@
 let flagSlider, unitInputBox
 let flagWidthBox, flagHeightBox
 let addFlagButton, addFlagButton2
+let bookWidthBox, bookHeightBox
 let colorPickerArray = []
 
-const flagArray = []
-const flags = []
-const bookDimensions = [3, 5] // [width, height] in inches
-const PPI = ((102 * 5 / 4.5) / 2).toFixed(0) // Half-scale
+const bookDimensions = [, ] // [width, height] in inches
+const PPI = ((102 * 5 / 4.5) / 2).toFixed(0) // Half-scale on 13" Macbook Pro Retina
 const scaleByPPI = x => {
   return x * PPI
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight)
+  createInterface()
+  updateInterface()
+}
+
+function draw() {
+  background(255)
+
+  bookDimensions[0] = bookWidthBox.value()
+  bookDimensions[1] = bookHeightBox.value()
+
+  addBookCover(...bookDimensions.map(scaleByPPI))
+
+  const flagHeight = unitInputBox.value() * flagSlider.value()
+  const gap = floor((bookDimensions[1] * PPI - colorPickerArray.length *
+    (flagHeight * PPI)) / (colorPickerArray.length + 1))
+
+  for (const [index, colorPicker] of colorPickerArray.entries()) {
+    colorPickerArray[index].position(0, gap * (index + 1) + index * flagHeight * PPI)
+    colorPickerArray[index].style('width', `${PPI * bookDimensions[0]}px`)
+    colorPickerArray[index].style('height', `${flagHeight * PPI}px`)
+  }
+
+  textSize(32)
+  fill(0);
+  text(flagHeight, width / 2, height - 80)
 }
 
 function createInterface() {
@@ -16,58 +44,27 @@ function createInterface() {
   unitInputBox = createInput(`${1/8}`, 'number')
   flagWidthBox = createInput(`${0}`, 'number')
   flagHeightBox = createInput(`${0}`, 'number')
+  bookWidthBox = createInput(`${3}`, 'number')
+  bookHeightBox = createInput(`${5}`, 'number')
 
   addFlagButton = createButton('Add Flag')
   addFlagButton.position(width - 100, height / 4)
   addFlagButton.size(90, 50)
-  addFlagButton.mousePressed(() => addFlag())
+  addFlagButton.mousePressed(() => colorPickerArray.push(createColorPicker()))
 }
 
 function updateInterface() {
+  updateElement(flagSlider, [width / 4, height - 40], ['width', `${width/2}px`])
 
-  updateUnitInputBox()
-  //updateFlagSlider()
-  updateInputBox(flagSlider, [width / 4, height - 40], ['width', `${width/2}px`])
-  updateInputBox(unitInputBox, [width / 4 - 70, height - 40], ['width', '60px'])
-  updateInputBox(flagHeightBox, [width - 100, height / 2 - 60], ['width', '60px'])
-  updateInputBox(flagWidthBox, [width - 100, height / 2 - 90], ['width', '60px'])
-  updateInputBox(addFlagButton, [width - 100, height / 4], ['width', '60px'])
-}
+  updateElement(unitInputBox, [width / 4 - 70, height - 40], ['width', '60px'])
 
-function setup() {
-  let canvas = createCanvas(windowWidth, windowHeight)
-  canvas.style('display', 'block')
+  updateElement(flagHeightBox, [width - 100, height / 2 - 60], ['width', '60px'])
+  updateElement(flagWidthBox, [width - 100, height / 2 - 90], ['width', '60px'])
 
-  createInterface()
-}
+  updateElement(addFlagButton, [width - 100, height / 4], ['width', '60px'])
 
-function draw() {
-  background(255)
-
-  addBookCover(...bookDimensions.map(scaleByPPI))
-
-  const flagHeight = unitInputBox.value() * flagSlider.value()
-  let gap = floor((5 * PPI - flags.length * (flagHeight * PPI)) / (flags.length + 1))
-
-  for (const [index, flag] of flags.entries()) {
-    flag.size(PPI * 5, flagHeight * PPI)
-    flag.position(0, gap * (index + 1) + index * flagHeight * PPI)
-    colorPickerArray[index].position(PPI * 5 + 10, gap * (index + 1) + index * flagHeight * PPI)
-    colorPickerArray[index].style('width', `${PPI * 5}px`)
-    colorPickerArray[index].style('height', `${flagHeight * PPI}px`)
-  }
-
-  updateInterface()
-
-  updateFlags(flagArray)
-
-  textSize(32)
-  fill(0);
-  text(flagHeight, width / 2, height - 80)
-}
-
-function updateFlags() {
-
+  updateElement(bookWidthBox, [width - 100, 50], ['width', '60px'])
+  updateElement(bookHeightBox, [width - 100, 75], ['width', '60px'])
 }
 
 function addBookCover(width, height) {
@@ -75,57 +72,12 @@ function addBookCover(width, height) {
   rect(0, 0, width, height)
 }
 
-function addFlag() {
-  const flag = createButton('')
-  const colorPicker = createColorPicker('black')
-
-  colorPicker.position(0, flags.length * height)
-
-  colorPickerArray.push(colorPicker)
-
-  flag.position(0, flags.length * height)
-  flag.size(flagWidthBox.value() * PPI, flagHeightBox.value() * PPI)
-  flag.mousePressed(() => {
-    let r = hex(floor(random(255)), 2)
-    let g = hex(floor(random(255)), 2)
-    let b = hex(floor(random(255)), 2)
-    flag.style('backgroundColor', `#${r}${g}${b}`)
-  })
-  flags.push(flag)
-}
-
-function addFlag(width, height) {
-  const flag = createButton('')
-  const colorPicker = createColorPicker()
-
-  colorPicker.position(width, flags.length * height)
-  colorPickerArray.push(colorPicker)
-  flag.position(0, flags.length * height)
-  flag.size(width, height)
-  flag.mousePressed(() => {
-    let r = hex(floor(random(255)), 2)
-    let g = hex(floor(random(255)), 2)
-    let b = hex(floor(random(255)), 2)
-    flag.style('backgroundColor', `#${r}${g}${b}`)
-  })
-  flags.push(flag)
-}
-
-function updateFlagSlider() {
-  flagSlider.position(width / 4, height - 40)
-  flagSlider.style('width', `${width/2}px`)
-}
-
-function updateUnitInputBox() {
-  unitInputBox.position(20, height - 40)
-  unitInputBox.style('width', `60px`)
-}
-
-function updateInputBox(inputBox, position, style) {
-  inputBox.position(...position)
-  inputBox.style(...style)
+function updateElement(element, position, style) {
+  element.position(...position)
+  element.style(...style)
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight)
+  updateInterface()
 }
